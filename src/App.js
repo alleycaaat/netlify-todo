35161e261ef488a48c477bda98bf0fca74f2d002
alleycaaat <achulslander@gmail.com>
2022-07-09 15:35:58 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const AddTaskForm = ({ addTask }) => {
+    const [value, setValue] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        value && addTask(value);
+        setValue('');
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                type='text'
+                value={value}
+                placeholder='Enter a title for this task…'
+                onChange={(e) => setValue(e.target.value)}
+            />
+            <button type='submit'>
+                <i class='fas fa-plus'></i>
+            </button>
+        </form>
+    );
+};
+
+const ToDoList = () => {
+    const [tasks, setTasks] = useState([
+        {
+            text: 'Like',
+            isCompleted: false,
+        },
+        {
+            text: 'Comment',
+            isCompleted: false,
+        },
+        {
+            text: 'Subscribe',
+            isCompleted: false,
+        },
+    ]);
+
+    const addTask = (text) => setTasks([...tasks, { text }]);
+
+    const toggleTask = (index) => {
+        const newTasks = [...tasks];
+        newTasks[index].isCompleted = !newTasks[index].isCompleted;
+        setTasks(newTasks);
+    };
+
+    const removeTask = (index) => {
+        const newTasks = [...tasks];
+        newTasks.splice(index, 1);
+        setTasks(newTasks);
+    };
+
+    return (
+        <div className='todo-list'>
+            {tasks.map((task, index) => (
+                <div className='todo'>
+                    <span
+                        onClick={() => toggleTask(index)}
+                        className={
+                            task.isCompleted
+                                ? 'todo-text todo-completed'
+                                : 'todo-text'
+                        }
+                    >
+                        {task.text}
+                    </span>
+                    <button onClick={() => removeTask(index)}>
+                        <i class='fas fa-trash-alt'></i>
+                    </button>
+                </div>
+            ))}
+            <AddTaskForm addTask={addTask} />
+        </div>
+    );
+};
+
 }
 
-export default App;
+export default App
